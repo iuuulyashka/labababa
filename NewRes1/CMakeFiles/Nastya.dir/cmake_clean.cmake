1b781491c4dbb9ace65c25fd3561fe06e7ec689b
iuuulyashka <145469118+iuuulyashka@users.noreply.github.com>
2024-02-24 17:20:07 +0200

    fails

---
+++ NewRes1/CMakeFiles/Nastya.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Nastya.dir/Laba_1.cpp.o"
+  "CMakeFiles/Nastya.dir/Laba_1.cpp.o.d"
+  "Nastya"
+  "Nastya.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Nastya.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ NewRes1/CMakeFiles/Nastya.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Nastya.
+# This may be replaced when dependencies are built.
